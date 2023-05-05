a2fb4c3841c1eeef2cf27163f68f38f292a1b7f2
AngelaKaye <130958588+AngelaKaye@users.noreply.github.com>
2023-05-06 01:23:47 +0800

    7

--- sw.js
+++ sw.js
@@ -1,16 +1,8 @@
 var cacheName = 'itst-pwa';
 var filesToCache = [
   '/',
-  '/home_page.html',
-  '/aboutme_page.html',
-  '/gallery_page.html',
-  '/hobbies_page.html',
-  '/likes_page.html',
-  '/css/page1style.css',
-  '/css/page2style.css',
-  '/css/page3style.css',
-  '/css/page4style.css',
-  '/css/page5style.css',
+  '/index.html',
+  '/css/style.css',
   '/js/main.js'
 ];
 
